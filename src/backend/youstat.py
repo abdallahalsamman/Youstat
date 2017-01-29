1c7d77735900c290d14b0363060689c5b8193bd9
abdullah samman <abdullah.samman@mokus.io>
2017-01-29 20:11:48 +0200

    #5 add stopwords for some langs; add word count support for more langs

--- src/backend/youstat.py
+++ src/backend/youstat.py
@@ -11,64 +11,17 @@ htmlParser = HTMLParser()
 # import ipdb; ipdb.set_trace()
 
 API_KEY = os.environ['YOUTUBE_API_KEY']
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # CHANNEL_NAME = "nigahiga" # CHANNEL WITHOUT AUTO SUBTITLES BUT WITH MANUAL SUBTITLES
 # CHANNEL_NAME = "KSIOlajidebt" # CHANNEL WITHOUT MANUAL SUBTITLES BUT WITH AUTO SUBTITLES
 
-PAGE_SIZE = 50 # get one vid only 50 max
-TOP_WORDS_SIZE = 30 # the top 30 frequent words
+PAGE_SIZE = 50
+if DEBUG:
+	PAGE_SIZE = 2
 
-stopwords = ['a', 'about', 'above', 'across', 'after', 'afterwards']
-stopwords += ['again', 'against', 'all', 'almost', 'alone', 'along']
-stopwords += ['already', 'also', 'although', 'always', 'am', 'among']
-stopwords += ['amongst', 'amoungst', 'amount', 'an', 'and', 'another']
-stopwords += ['any', 'anyhow', 'anyone', 'anything', 'anyway', 'anywhere']
-stopwords += ['are', 'around', 'as', 'at', 'back', 'be', 'became']
-stopwords += ['because', 'become', 'becomes', 'becoming', 'been']
-stopwords += ['before', 'beforehand', 'behind', 'being', 'below']
-stopwords += ['beside', 'besides', 'between', 'beyond', 'bill', 'both']
-stopwords += ['bottom', 'but', 'by', 'call', 'can', 'cannot', 'cant']
-stopwords += ['co', 'computer', 'con', 'could', 'couldnt', 'cry', 'de']
-stopwords += ['describe', 'detail', 'did', 'do', 'done', 'dont', 'down', 'due']
-stopwords += ['during', 'each', 'eg', 'eight', 'either', 'eleven', 'else']
-stopwords += ['elsewhere', 'empty', 'enough', 'etc', 'even', 'ever']
-stopwords += ['every', 'everyone', 'everything', 'everywhere', 'except']
-stopwords += ['few', 'fifteen', 'fifty', 'fill', 'find', 'fire', 'first']
-stopwords += ['five', 'for', 'former', 'formerly', 'forty', 'found']
-stopwords += ['four', 'from', 'front', 'full', 'further', 'get', 'give']
-stopwords += ['go', 'had', 'has', 'hasnt', 'have', 'he', 'hence', 'her']
-stopwords += ['here', 'hereafter', 'hereby', 'herein', 'hereupon', 'hers']
-stopwords += ['herself', 'him', 'himself', 'his', 'how', 'however']
-stopwords += ['hundred', 'i', 'im', 'ive', 'ie', 'if', 'in', 'inc', 'indeed']
-stopwords += ['interest', 'into', 'is', 'it', 'its', 'itself', 'just', 'keep']
-stopwords += ['last', 'latter', 'latterly', 'least', 'less', 'ltd', 'made']
-stopwords += ['many', 'may', 'me', 'meanwhile', 'might', 'mill', 'mine']
-stopwords += ['more', 'moreover', 'most', 'mostly', 'move', 'much']
-stopwords += ['must', 'my', 'myself', 'name', 'namely', 'neither', 'never']
-stopwords += ['nevertheless', 'next', 'nine', 'no', 'nobody', 'none']
-stopwords += ['noone', 'nor', 'not', 'nothing', 'now', 'nowhere', 'of']
-stopwords += ['off', 'often', 'on','once', 'one', 'only', 'onto', 'or']
-stopwords += ['other', 'others', 'otherwise', 'our', 'ours', 'ourselves']
-stopwords += ['out', 'over', 'own', 'part', 'per', 'perhaps', 'please']
-stopwords += ['put', 'rather', 're', 's', 'same', 'see', 'seem', 'seemed']
-stopwords += ['seeming', 'seems', 'serious', 'several', 'she', 'should']
-stopwords += ['show', 'side', 'since', 'sincere', 'six', 'sixty', 'so']
-stopwords += ['some', 'somehow', 'someone', 'something', 'sometime']
-stopwords += ['sometimes', 'somewhere', 'still', 'such', 'system', 'take']
-stopwords += ['ten', 'than', 'that', 'the', 'their', 'them', 'themselves']
-stopwords += ['then', 'thence', 'there', 'thereafter', 'thereby']
-stopwords += ['therefore', 'therein', 'thereupon', 'these', 'they']
-stopwords += ['thick', 'thin', 'third', 'this', 'those', 'though', 'three']
-stopwords += ['three', 'through', 'throughout', 'thru', 'thus', 'to']
-stopwords += ['together', 'too', 'top', 'toward', 'towards', 'twelve']
-stopwords += ['twenty', 'two', 'un', 'under', 'until', 'up', 'upon']
-stopwords += ['us', 'very', 'via', 'was', 'we', 'well', 'were', 'what']
-stopwords += ['whatever', 'when', 'whence', 'whenever', 'where']
-stopwords += ['whereafter', 'whereas', 'whereby', 'wherein', 'whereupon']
-stopwords += ['wherever', 'whether', 'which', 'while', 'whither', 'who']
-stopwords += ['whoever', 'whole', 'whom', 'whose', 'why', 'will', 'with']
-stopwords += ['within', 'without', 'would', 'yet', 'you', 'your']
-stopwords += ['yours', 'youre', 'yourself', 'yourselves']
+TOP_WORDS_SIZE = 30 # the top 30 frequent words
+STOPWORDS_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/" + "stopwords"
 
 def channel_url(name):
 	return "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername="+name+"&key="+API_KEY
@@ -83,22 +36,18 @@ def playlist_url(id, token):
 def uploads_id(channel):
 	return channel['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
-def latest_en_sub(videos_manual_subs, videos_manual_subs_translated,\
- 				videos_auto_subs, videos_auto_subs_translated):
-	if videos_manual_subs:
-		return videos_manual_subs[0][1][1]
-	if videos_auto_subs:
-		return videos_auto_subs[0][1][1]
-	if videos_manual_subs_translated:
-		return videos_manual_subs_translated[0][2][1]
-	if videos_auto_subs_translated:
-		return videos_auto_subs_translated[0][2][1]
-
 def video_id(vid):
 	return vid['contentDetails']['videoId']
 
 def is_video(vid):
 	return ('videoId' in vid['contentDetails'])
+
+def extract_original_subs(manual_subs, manual_subs_non_en, auto_subs, auto_subs_non_en):
+	return map(lambda x: x[1], manual_subs + auto_subs + manual_subs_non_en + auto_subs_non_en)
+
+def extract_english_subs(manual_subs, manual_subs_non_en, auto_subs, auto_subs_non_en):
+	return map(lambda x: x[1], manual_subs + auto_subs) + \
+		map(lambda x: x[2], manual_subs_non_en + auto_subs_non_en)
 
 def format_subtitles(subtitles):
 	subtitles = htmlParser.unescape(htmlParser.unescape(subtitles))
@@ -182,19 +131,32 @@ def get_subtitle_statistics(sub):
 
 def get_playlist(channel, token=None):
 	playlist = get_json(playlist_url(uploads_id(channel), token))
+	if DEBUG:
+		return playlist['items']
 	if 'nextPageToken' in playlist:
 		return playlist['items'] + get_playlist(channel, playlist['nextPageToken'])
 	else:
 		return playlist['items']
 
-def words_frequency(manual_subs, auto_subs):
+def words_frequency(subtitles):
 	def sort_words_by_freq(words):
 		return sorted(words.items(), key=lambda v: v[1], reverse=True)
 
+	def get_stopwords(lang):
+		def stopwords_file():
+			return STOPWORDS_FOLDER+'/'+lang+'.txt'
+		supported_langs = [name.split('.')[0] for name in os.listdir(STOPWORDS_FOLDER)]
+		if lang in supported_langs:
+			with open( stopwords_file() ) as f:
+			    content = f.read().splitlines()
+			return content
+		else:
+			return []
+
 	def rm_stop_words(subtitle):
-		regex = r'\b('+'|'.join(stopwords)+r')\b'
-		sub_clean_1 = re.sub('[?!,.\(\)\[\]]', ' ', subtitle)
-		sub_clean_2 = re.sub('[\'"-]', '', sub_clean_1)
+		regex = r'\b('+'|'.join( get_stopwords(  subtitle[0] ) )+r')\b'
+		sub_clean_1 = re.sub('[?!,.\(\)\[\]]', ' ', subtitle[1])
+		sub_clean_2 = re.sub('["-]', '', sub_clean_1)
 		clean_subtitle = re.sub(regex, '', sub_clean_2, flags=re.IGNORECASE)
 		return clean_subtitle
 
@@ -209,12 +171,12 @@ def words_frequency(manual_subs, auto_subs):
 
 	def process_subs(subtitles):
 		words = {}
-		for id, subtitle in subtitles:
+		for subtitle in subtitles:
 			clean_subtitle = rm_stop_words(subtitle)
 			words = count_words(clean_subtitle, words)
 		return words
 
-	words = process_subs(manual_subs + auto_subs)
+	words = process_subs(subtitles)
 	return sort_words_by_freq(words)[0:TOP_WORDS_SIZE]
 
 def beautify_stats(stats):
@@ -239,9 +201,10 @@ def get_stats(channel_name):
 		split_results([get_auto_sub(i) for i in video_ids_no_manual_subs]) )
 
 	if manual_subs or auto_subs or manual_subs_non_en or auto_subs_non_en:
-		# frequent_words = words_frequency(manual_subs, manual_subs_non_en, auto_subs, auto_subs_non_en)
-		sub = latest_en_sub(manual_subs, manual_subs_non_en, auto_subs, auto_subs_non_en)
-		stats = get_subtitle_statistics(sub)
+		original_subs = extract_original_subs(manual_subs, manual_subs_non_en, auto_subs, auto_subs_non_en)
+		english_subs = extract_english_subs(manual_subs, manual_subs_non_en, auto_subs, auto_subs_non_en)
+		frequent_words = words_frequency( original_subs )
+		stats = get_subtitle_statistics( english_subs[0][1] )
 		beautiful_stats = beautify_stats(stats)
 		return beautiful_stats
 	else:
